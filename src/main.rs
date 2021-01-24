639aba61291f32ab65eaf422a2e527ea1f87f75e
Alessio Proietti <alessio.proietti@protonmail.com>
2021-01-24 12:09:40 +0100

    Queue imported and working.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use queue::Queue;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    let mut q = Queue::new();
+
+    q.queue("Alessio dice: ".as_bytes()).unwrap();
+    q.queue("ciao amici,".as_bytes()).unwrap();
+    q.queue("scrivo dalla coda".as_bytes()).unwrap();
+
+    while let Some(item) = q.dequeue() {
+        println!("{}", str::from_utf8(&item).unwrap());
+    }
 }
