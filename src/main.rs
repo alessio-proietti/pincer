64768e463ffe636653d945536bd7f44a4dc793de
Alessio Proietti <alessio.proietti@protonmail.com>
2021-01-24 14:20:05 +0100

    simulating hashmap with arbitrary large objects

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use queue::Queue;
+use std::collections::HashMap;
 use std::str;
+use std::vec::Vec;
 
 fn main() {
-    let mut q = Queue::new();
+    //let mut q = Queue::new();
+    let mut vec = Vec::new();
 
-    q.queue("Alessio dice: ".as_bytes()).unwrap();
-    q.queue("ciao amici,".as_bytes()).unwrap();
-    q.queue("scrivo dalla coda".as_bytes()).unwrap();
+    let mut map = HashMap::new();
+    vec.push(Queue::new());
+    map.insert("hello", 0);
 
-    while let Some(item) = q.dequeue() {
+    vec[map["hello"]].queue("Alessio dice: ".as_bytes()).unwrap();
+    vec[map["hello"]].queue("ciao amici,".as_bytes()).unwrap();
+    vec[map["hello"]].queue("scrivo dalla coda".as_bytes()).unwrap();
+
+    while let Some(item) = vec[map["hello"]].dequeue() {
         println!("{}", str::from_utf8(&item).unwrap());
     }
 }
