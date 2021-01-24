299d9a181199ba7332c62c5853cb3f589f5ca8d2
Alessio Proietti <alessio.proietti@protonmail.com>
2021-01-24 17:22:35 +0100

    a first version of the broker has been hacked.

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,40 @@ use std::collections::HashMap;
 use std::str;
 use std::vec::Vec;
 
+struct Broker<'a> {
+    channels: HashMap<&'a str, usize>,
+    indexes: Vec<queue::Queue<&'a str>>,
+}
+
+impl<'a> Broker<'a> {
+    
+    pub fn publish(&mut self, channel: &'a str, payload: &'a str) {
+        self.indexes[self.channels[channel]].queue(payload);
+    }
+
+    pub fn create_channel(&mut self, channel: &'a str) {
+        self.indexes.push(Queue::new());
+        self.channels.insert(channel, self.indexes.len()-1);
+    }
+
+}
+
 fn main() {
-    //let mut q = Queue::new();
-    let mut vec = Vec::new();
 
-    let mut map = HashMap::new();
-    vec.push(Queue::new());
-    map.insert("hello", 0);
+    let mut broker = Broker{
+        channels: HashMap::new(),
+        indexes: Vec::new()
+    };
 
-    vec[map["hello"]].queue("Alessio dice: ".as_bytes()).unwrap();
-    vec[map["hello"]].queue("ciao amici,".as_bytes()).unwrap();
-    vec[map["hello"]].queue("scrivo dalla coda".as_bytes()).unwrap();
+    broker.create_channel("hello");   
+    broker.publish("hello", "Alessio ");  
+    broker.publish("hello", "Proietti ");  
+    broker.publish("hello", "mangia ");  
+    broker.publish("hello", "gli ");  
+    broker.publish("hello", "spaghetti");  
 
-    while let Some(item) = vec[map["hello"]].dequeue() {
-        println!("{}", str::from_utf8(&item).unwrap());
+
+    while let Some(item) = broker.indexes[broker.channels["hello"]].dequeue() {
+        println!("{}", item);
     }
 }
